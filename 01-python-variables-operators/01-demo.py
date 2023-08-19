617d71e82e190067a6edf6667bdaa11918fdf236
lion tech academy <info@liontechacademy.comn>
2023-08-19 11:53:42 -0400

    python intro repo

---
+++ 01-python-variables-operators/01-demo.py
@@ -0,0 +1,15 @@
+price  =  100   #interger   [ numbers in python are referred to an integer]
+products  =  "cars"   #word letters are referred to as strings
+
+user_age, user_name   =  30,  'peter'
+user_age  =  40 
+user_name =  'peter'
+#40  = 'elvis'   #this proves that a variable cant start with a number
+#price   =  "4000"
+#2username  = 'elvis'
+user_name_2    = "paul"
+User_Name     =  "elvis"
+
+#3 * 7     =  21 
+#in python  
+user_name_country   =  "cameroon, USA, United Kingdom, cameroon, ghana"
---
+++ 01-python-variables-operators/price.py
@@ -0,0 +1,8 @@
+unit  =  1000
+price =  500 
+
+print (price)
+print (unit)
+
+total_revenue = price * unit
+print (total_revenue)
